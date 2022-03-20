34eb6d050fd1235d6a53c3c2241244cdfc8c45c5
Chau Pham <ncp9988@gmail.com>
2022-03-20 13:17:50 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -37,10 +37,10 @@ db.once('open', () => {
   });
 });
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/redux-store',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost:27017/redux-store',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+// );
