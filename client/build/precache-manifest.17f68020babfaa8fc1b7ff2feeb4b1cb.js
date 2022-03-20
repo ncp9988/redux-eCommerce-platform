6291fb648531518c16c1fe8a7dd5f83d45951fe5
Chau Pham <ncp9988@gmail.com>
2022-03-20 12:09:21 -0500

    finish up

---
+++ client/build/precache-manifest.17f68020babfaa8fc1b7ff2feeb4b1cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ae2be390588edd555a9b1ec26b46af",
+    "url": "/redux-eCommerce-platform/index.html"
+  },
+  {
+    "revision": "bec302bfb59c83c6034b",
+    "url": "/redux-eCommerce-platform/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "de7124e00f252ee379c4",
+    "url": "/redux-eCommerce-platform/static/js/2.fe5028fc.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/redux-eCommerce-platform/static/js/2.fe5028fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bec302bfb59c83c6034b",
+    "url": "/redux-eCommerce-platform/static/js/main.cb7b08f3.chunk.js"
+  },
+  {
+    "revision": "f017dfd1913d574e91f6",
+    "url": "/redux-eCommerce-platform/static/js/runtime-main.0b10e83f.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/redux-eCommerce-platform/static/media/spinner.ab497855.gif"
+  }
+]);
